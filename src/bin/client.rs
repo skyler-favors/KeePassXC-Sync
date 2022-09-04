2383ac59eeb99d8bcfe29ff146b980869f6ec759
sky-86 <sky86one@gmail.com>
2022-09-04 14:45:02 -0500

    have basic structure setup, need to finish merging process.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use anyhow::{Result, Error};
+use async_fs::read;
+use reqwest::Client;
+
+async fn send_db(url: &str, filename: &str) -> Result<(), Error> {
+    let filepath = format!("./{}.kdbx", filename);
+    let bytes = read(&filepath).await?;
+
+    let client = Client::new();
+    let res = client.post(url)
+        .body(bytes)
+        .send()
+        .await?;
+
+    // wait for response from server
+    println!("{:?}", res);
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "http://127.0.0.1:8080";
+    let filename = "password";
+
+    send_db(url, filename).await?;
+    Ok(())
+}
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,2 +1,57 @@
+use actix_web::Error as ActError;
+use actix_web::{
+    error::ErrorBadRequest,
+    web::{post, BytesMut, Payload},
+    App, HttpResponse, HttpServer,
+};
+use anyhow::{Error as AnyError, Result};
+use async_fs::File;
+use futures::{AsyncWriteExt, StreamExt};
 
-fn main() {}
+const MAX_SIZE: usize = 262_144; // 256k
+
+async fn merge_request(payload: Payload) -> Result<HttpResponse, ActError> {
+    // save the recieved db
+    write_db(payload).await?;
+
+    // merge the two db's
+    merge().await?;
+
+    // send the merged db back
+    Ok(HttpResponse::Ok().finish())
+}
+
+async fn write_db(mut payload: Payload) -> Result<(), ActError> {
+    let mut body = BytesMut::new();
+
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+
+        // limit the max size of in-memory payload
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk)
+    }
+    // body should now be the full database
+    let filename = "new_db";
+    let path = format!("./{}.kdbx", filename);
+    let mut file = File::create(&path).await?;
+    file.write_all(&body).await?;
+
+    Ok(())
+}
+
+async fn merge() -> Result<(), ActError> {
+    Ok(())
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), AnyError> {
+    HttpServer::new(|| App::new().route("/", post().to(merge_request)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await?;
+
+    Ok(())
+}
